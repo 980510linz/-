21588c37a987cb18f5b791c45a46e2c8d301c9a5
網站小編-KAI <denny980510@gmail.com>
2026-09-11 23:04:20 +0800

    Add files via upload

---
+++ analytics-config.example.js
@@ -0,0 +1,9 @@
+// 參考用：不要覆蓋你已設定好的 analytics-config.js。
+window.SIGNWELL_ANALYTICS = {
+  enabled: true,
+  endpoint: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
+};
+window.SIGNWELL_NEWSLETTER = {
+  enabled: true,
+  endpoint: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
+};
