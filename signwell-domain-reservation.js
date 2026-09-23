df073211f19ece46b16be3b04e3914a68f3b0b5f
網站小編-KAI <denny980510@gmail.com>
2026-09-23 17:58:44 +0800

    Add files via upload

---
+++ signwell-domain-reservation.js
@@ -0,0 +1,7 @@
+/* SIGN WELL custom domain is active. GitHub Pages remains the origin behind signwell.com.tw. */
+window.SIGNWELL_DOMAIN_RESERVATION=Object.freeze({
+  currentPublicBase:'https://signwell.com.tw/',
+  legacyGithubBase:'https://980510linz.github.io/-/',
+  futureCustomDomain:'https://signwell.com.tw/',
+  customDomainEnabled:true
+});
